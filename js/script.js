a762f9e4045b07a192befe1d074c73f350ea4fd1
giuseppedonnici <giuseppe.donnici8@gmail.com>
2023-03-14 18:37:28 +0100

    Add: Bonus 2 finished

--- js/script.js
+++ js/script.js
@@ -36,9 +36,27 @@ for (let i = 0; i < imagesArray.length; i++) {
     const sliderItem = `
         <div class="item">
         <img src="${currentImage}" alt="">
+        <div class="thumb-container">
+                        <div class="thumb">
+                            <img src="img/01.jpg" alt="">
+                        </div>
+                        <div class="thumb">
+                            <img src="img/02.jpg" alt="">
+                        </div>
+                        <div class="thumb">
+                            <img src="img/03.jpg" alt="">
+                        </div>
+                        <div class="thumb">
+                            <img src="img/04.jpg" alt="">
+                        </div>
+                        <div class="thumb">
+                            <img src="img/05.jpg" alt="">
+                        </div>
+                    </div>
+
         </div>`;
     itemsContainer.innerHTML += sliderItem;
-    
+
 }
 
 // Ora ho tutti gli items inseriti nella pagina tramite JS ma in display none
@@ -46,9 +64,13 @@ for (let i = 0; i < imagesArray.length; i++) {
 
 const itemsArray = document.getElementsByClassName("item");
 console.log(itemsArray);
+const thumbsArray = document.getElementsByClassName("thumb");
+console.log(thumbsArray);
 
 let activeItemIndex = 0;
 itemsArray[activeItemIndex].classList.add("active");
+let activeThumbIndex = 0;
+thumbsArray[activeThumbIndex].classList.add("active");
 
 // A questo punto è visibile solo il primo itemsArray, devo andare a gestire la classe active
 // spostando l'indice
@@ -59,23 +81,30 @@ const prevBtn = document.querySelector(".prev");
 console.log(nextBtn, prevBtn);
 
 // Cosa succede quando clicco sul bottone next?
-nextBtn.addEventListener("click", function() {
+nextBtn.addEventListener("click", function () {
 
     // togli la classe hidden al bottone prev
     // prevBtn.classList.remove("hidden"); (***PARTE DEL MILESTONE 3***)
 
-    if (activeItemIndex === itemsArray.length -1) {
+    if (activeItemIndex === itemsArray.length - 1) {
         itemsArray[activeItemIndex].classList.remove("active");
+        thumbsArray[activeThumbIndex].classList.remove("active");
         activeItemIndex = 0;
+        activeThumbIndex = 0;
         itemsArray[activeItemIndex].classList.add("active");
-    } else if (activeItemIndex < (itemsArray.length -1)) {
+        thumbsArray[activeThumbIndex].classList.add("active");
+    } else if (activeItemIndex < (itemsArray.length - 1)) {
         // Se l'indice dell'item attuale è minore della lunghezza dell'array 
         // rimuovi la classe active
         itemsArray[activeItemIndex].classList.remove("active");
+        thumbsArray[activeThumbIndex].classList.remove("active");
         // incrementa di 1 l'indice attuale
         activeItemIndex++;
+        activeThumbIndex = activeThumbIndex + 6;
         // aggiungi la classe active al nuovo item
         itemsArray[activeItemIndex].classList.add("active");
+        thumbsArray[activeThumbIndex].classList.add("active");
+        console.log(activeThumbIndex)
 
         // Se sei arrivato all'ultimo item (***PARTE DEL MILESTONE 3***)
         // if (activeItemIndex === itemsArray.length - 1) {
@@ -86,26 +115,32 @@ nextBtn.addEventListener("click", function() {
 });
 
 // Cosa succede quando clicco sul bottone prev?
-prevBtn.addEventListener("click", function() {
+prevBtn.addEventListener("click", function () {
 
     // Se ti trovi alla prima immagine
     if (activeItemIndex === 0) {
         // togli la classe active dall'item corrente
         itemsArray[activeItemIndex].classList.remove("active");
+        thumbsArray[activeThumbIndex].classList.remove("active");
         // assegna l'indico dell'ultimo elemento dell'array
         activeItemIndex = itemsArray.length - 1;
+        activeThumbIndex = thumbsArray.length - 1;
         // aggiungi la classe active
         itemsArray[activeItemIndex].classList.add("active");
+        thumbsArray[activeThumbIndex].classList.add("active");
     } else {
         // togli la classe hidden dal bottone next
         // nextBtn.classList.remove("hidden"); (***PARTE DEL MILESTONE 3***)
         // togli la classe active dall'item corrente
         itemsArray[activeItemIndex].classList.remove("active");
+        thumbsArray[activeThumbIndex].classList.remove("active");
         // decrementa l'indice di 1
         activeItemIndex--;
+        activeThumbIndex = activeThumbIndex -6;
         // aggiungi la classe active all'item corrente
         itemsArray[activeItemIndex].classList.add("active");
-    
+        thumbsArray[activeThumbIndex].classList.add("active");
+
         // Se sei arrivato al primo item (***PARTE DEL MILESTONE 3***)
         // if (activeItemIndex === 0) {
         //     prevBtn.classList.add("hidden");
